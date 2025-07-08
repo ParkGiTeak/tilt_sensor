cc9142c27a72138d04623beee2c89b0f65e23f71
ParkGiTeak <rl980901@naver.com>
2025-07-08 21:29:36 +0900

    feat: AceelerometerEvent로 들어온 data의 x, y값을 Positioned Widget의 left, right Atrribute에 반영

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:sensors_plus/sensors_plus.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,23 +26,43 @@ class SensorApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Orientation LandScape
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeLeft,
+    ]);
+
+    // MediaQuery의 size로 디바이스 화면의 width, height를 얻을 수 있다.
     final centerX = MediaQuery.of(context).size.width / 2 - 50;
     final centerY = MediaQuery.of(context).size.height / 2 - 50;
 
     return Scaffold(
       body: Stack(
         children: [
-          Positioned(
-            left: centerX,
-            top: centerY,
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.green, // Decoration 사용중이면 Container의 Color Attribute를 제거한다.
-                shape: BoxShape.circle,
-              ),
-              width: 100,
-              height: 100,
-            ),
+          StreamBuilder<AccelerometerEvent>(
+            stream: accelerometerEventStream(),
+            builder: (context, asyncSnapshot) {
+              if (!asyncSnapshot.hasData) {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+
+              final event = asyncSnapshot.data!;
+
+              return Positioned(
+                left: centerX + event.y * 20,
+                top: centerY + event.x * 20,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                    // Decoration 사용중이면 Container의 Color Attribute를 제거한다.
+                    shape: BoxShape.circle,
+                  ),
+                  width: 100,
+                  height: 100,
+                ),
+              );
+            },
           ),
         ],
       ),
